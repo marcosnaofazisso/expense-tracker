15bf2bd9fae2684ad8be3fa043fafad9ebebadc1
Marcos Ferreira <marcosferreiraf22@gmail.com>
2021-10-06 16:20:57 -0300

    basic react structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as C from './App.styles'
+
+
+function App() {
+  return (
+    <div>
+      <C.Container>
+        ...
+      </C.Container>
+    </div>
+  )
+}
+
+export default App;
